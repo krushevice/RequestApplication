9a80cf362ba99124586b56dcf2a050789eaa4a58
BychkouA <BychkouA123@123.by>
2017-03-21 17:04:19 +0300

    change user

--- springmvctest1/src/main/java/by/sancho/springmvctest/HomeController.java
+++ springmvctest1/src/main/java/by/sancho/springmvctest/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("user", "sancho" );
+		model.addAttribute("user", "sancho-pancho" );
 		
 		return "home";
 	}
