7cc810be928f8bab63477510fafd198d46d7725b
BychkouA <BychkouA123@123.by>
2017-04-17 17:13:14 +0300

    hibernate test

--- RequestApp/src/main/java/by/iba/requestApp/daoImpl/UserDaoImpl.java
+++ RequestApp/src/main/java/by/iba/requestApp/daoImpl/UserDaoImpl.java
@@ -3,10 +3,24 @@ package by.iba.requestApp.daoImpl;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+
 import javax.sql.DataSource;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 import by.iba.requestApp.dao.UserDao;
+import by.iba.requestApp.viewBean.LoginBean;
 
 public class UserDaoImpl implements UserDao {
+	
+	SessionFactory sessionFactory = new Configuration()
+            .configure() // configures settings from hibernate.cfg.xml
+            .buildSessionFactory();
 
 	DataSource dataSource;
 
@@ -20,8 +34,21 @@ public class UserDaoImpl implements UserDao {
 
 	@Override
 	public boolean isValidUser(String username, String password) throws SQLException {
-		
-		  String query = "Select count(1) from user where username = ? and password = ?";
+				
+		Session session = sessionFactory.openSession();
+	    
+	    Query query = session.createQuery("from LoginBean where username = :username and password = :password");
+		query.setParameter("username", username);
+		query.setParameter("password", password);
+		List list = query.list();
+		session.close();
+		if(list.size()>0){
+			return true;
+		}
+
+		return false;
+				
+		  /*String query = "Select count(1) from user where username = ? and password = ?";
 		  PreparedStatement pstmt = dataSource.getConnection().prepareStatement(query);
 		  pstmt.setString(1, username); 
 		  pstmt.setString(2, password); 
@@ -29,7 +56,7 @@ public class UserDaoImpl implements UserDao {
 		  if(resultSet.next()) 
 			  return (resultSet.getInt(1) > 0); 
 		  else 
-			  return false;
+			  return false;*/
 		 
 		/*if (username.equals("admin") && password.equals("135")) {
 			return true;
@@ -40,8 +67,27 @@ public class UserDaoImpl implements UserDao {
 
 	@Override
 	public boolean insertUser(String username, String password) throws SQLException {
+/*		System.out.println("daoImpl insertUser");
+		SessionFactory sessionFactory;
+	    sessionFactory = new Configuration()
+	            .configure() // configures settings from hibernate.cfg.xml
+	            .buildSessionFactory();*/
 
-		try {
+	    Session session = sessionFactory.openSession();
+	    Transaction tx = session.beginTransaction();
+	    int id = lastId(session)+1;
+	   
+	    LoginBean lb = new LoginBean();
+	    lb.setId(id);
+	    lb.setUsername(username);
+	    lb.setPassword(password);
+	    session.save(lb);
+	    tx.commit();
+	    session.close();
+	    return true;
+
+
+		/*try {
 			String query = "insert into user values(?,?,?)";
 			PreparedStatement pstmt = dataSource.getConnection().prepareStatement(query);
 			//тут достать последний id в таблице
@@ -52,8 +98,27 @@ public class UserDaoImpl implements UserDao {
 			return true;
 		} catch (Exception e) {
 			return false;
-		} 
+		} */
 		
+	}
+	
+	public int lastId(Session session) throws SQLException{
+		String querySQL = "select max(id) from LoginBean";
+		Query query = session.createQuery(querySQL);
+		List list = query.list();
+		if(list.size()>0){
+			return (Integer) list.get(0);
+		}
+		return -1;
+	
+		 /*String query = "Select max(id) from user";
+		 PreparedStatement pstmt = dataSource.getConnection().prepareStatement(query);
+		 ResultSet resultSet = pstmt.executeQuery();
+		 if(resultSet.next()) {
+			 return resultSet.getInt(1); 
+		 }
+		 else 
+			 return -1;*/
 	}
 
 }
--- RequestApp/src/main/java/by/iba/requestApp/viewBean/LoginBean.java
+++ RequestApp/src/main/java/by/iba/requestApp/viewBean/LoginBean.java
@@ -1,10 +1,36 @@
 package by.iba.requestApp.viewBean;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
 public class LoginBean {
 
+	@Id
+	//@GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+	private int id;
+
+	@Column(name="username")
 	private String username;
+	
+	@Column(name="password")
 	private String password;
 
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	public String getUsername() {
 		return username;
 	}
